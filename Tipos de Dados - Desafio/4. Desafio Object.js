720ccc7b514fc42642b9ea3ca59fd107b482cadd
Alex Miranda <alex_miranda@outlook.com.br>
2023-01-29 20:47:04 -0300

    exercicio 4 finalizado

--- Tipos de Dados - Desafio/4. Desafio Object.js
+++ Tipos de Dados - Desafio/4. Desafio Object.js
@@ -1,2 +1,4 @@
 // crie um objeto chamado Carro que contenha chaves como: placa, 
 // ano e cor;
+const Carro = { placa: "QKB 1G59", ano: 2015, cor: "branco"}
+console.log("A placa do carro é: " + Carro.placa)
