094ce18d004756dd8bff3cb318217c2dff3b78c2
Alex Miranda <alex_miranda@outlook.com.br>
2023-01-30 21:52:55 -0300

    Exercicio 1 concluído

--- Funções - Desafios/1 - Função e variáveis.js
+++ Funções - Desafios/1 - Função e variáveis.js
@@ -1,2 +1,12 @@
 /** Crie uma função que leia 2 variáveis do tipo number fora de seu escopo e 
 que retorne a soma das mesmas. */
+
+const n1 = 5
+const n2 = 8
+
+function soma(n1,n2) {
+    return n1 + n2
+}
+console.log(soma(n1,n2))
+// let resultado = soma(n1,n2)
+// console.log(resultado)
