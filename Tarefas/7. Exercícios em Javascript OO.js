cc96e7817a13d38ef3bc7244c4488ec1bc509e55
Alex Miranda <alex_miranda@outlook.com.br>
2023-02-23 20:24:51 -0300

    Classe Lampada

--- Tarefas/7. Exercícios em Javascript OO.js
+++ Tarefas/7. Exercícios em Javascript OO.js
@@ -7,6 +7,33 @@ retornar eles em um console ou fazer algo que vocês queiram.
 Criatividade conta! 
 */
 
-class lampada {
-    
+class Lampada {
+
+    #acesa
+
+
+    constructor() {
+        this.#acesa = false;
+    }
+
+    acende(){
+        this.#acesa = true;
+    }
+
+    apaga(){
+        this.#acesa = false;
+    }
+
+    get acesa(){
+        return this.#acesa;
+    }
 }
+
+const minhaLampada = new Lampada();
+console.log(minhaLampada.acesa); //false
+
+minhaLampada.acende();
+console.log(minhaLampada.acesa); //true
+
+minhaLampada.apaga();
+console.log(minhaLampada.acesa); //false
