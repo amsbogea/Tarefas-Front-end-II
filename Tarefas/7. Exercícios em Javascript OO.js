a307f6513af4f6f2d2113bcb5988e199df233ad6
Alex Miranda <alex_miranda@outlook.com.br>
2023-02-22 22:30:48 -0300

    Praticando Javascript

---
+++ Tarefas/7. Exercícios em Javascript OO.js
@@ -0,0 +1,12 @@
+/*
+Crie uma classe que tenha no mínimo 2 atributos 
+e crie pelo menos 1 comportamento (método/função) 
+que faça algo com esses atributos, 
+retornar eles em um console ou fazer algo que vocês queiram.
+
+Criatividade conta! 
+*/
+
+class lampada {
+    
+}
